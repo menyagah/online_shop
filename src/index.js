6c7176de22a1bd7107e6a4b9e7b7a20134cf900a
menyagah <menyagah27@gmail.com>
2023-11-27 13:50:39 +0300

    Require newly created server and listen on port 8100

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-const http = require('http');
+const app = require('./server');
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    res.statusCode = 200;
-    res.end('Welcome to our home page');
-  }
-});
-
-server.listen(8100, () => console.log('Server is listening on port 8100...'));
+app.listen(8100, () => console.log('Server is listening on port 8100...'));
