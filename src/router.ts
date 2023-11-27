043ce30cf061f6aa1848b43c10c28bc337709279
menyagah <menyagah27@gmail.com>
2023-11-27 18:08:02 +0300

    Create and import application Router

---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import {Router} from 'express';
+
+
+const router = Router();
+
+
+/**
+ * Product routes
+ */
+router.get('/product', ()=>{})
+router.get('/product/:id', ()=>{})
+router.post('/product', ()=>{})
+router.delete('/product/:id', ()=>{})
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import router from './router';
 
 const app = express();
 
