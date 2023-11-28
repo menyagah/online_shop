c1b826c1cb60243476c5620dfb7caaaf9f1a077e
menyagah <menyagah27@gmail.com>
2023-11-28 11:14:13 +0300

    Validate bearer token

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -11,9 +11,22 @@ export const createJwt = (user) => {
     return token;
 };
 
-export const protect = (req, res)=>{
+export const protect = (req, res, next)=>{
     const bearer = req.headers.authorization;
     if(!bearer || !bearer.startsWith('Bearer ')){
-        return res.status(401).json('not authorized').end();
+        return res.status(401).json({message:'not authorized'}).end();
+    }
+
+    const [, token] = bearer.split(' ')
+    if(!token){
+        return res.status(401).json({message:'not authorized'}).end();
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        return next();
+    }catch(err){
+        return res.status(401).json({message:'not authorized'}).end();
     }
 }
