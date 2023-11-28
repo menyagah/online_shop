77d89b8a4c02880ba9657e243529f20913122db2
menyagah <menyagah27@gmail.com>
2023-11-28 09:25:40 +0300

    Install cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './router';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
