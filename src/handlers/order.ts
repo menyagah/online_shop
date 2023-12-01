0ba53aca28b86b3d991896e63db35ebc41a4d508
menyagah <menyagah27@gmail.com>
2023-12-01 11:52:05 +0300

    Delete commented code

--- src/handlers/order.ts
+++ src/handlers/order.ts
@@ -16,37 +16,6 @@ export const getOrders = async(req, res) => {
 
 }
 
-// export const createOrder = async(req, res) => {
-//     const order = await prisma.order.create({
-//         data: {
-//             completed: false,
-//             userId: req.body.id,
-//             product: req.body.id
-//         }
-//     })
-//     res.json({data: order})
-// }
-
-// export const createOrder = async (req, res) => {
-//     try {
-//       const { userId, productId } = req.body; // Assuming userId and productIds are part of the request body
-//       const newOrder = await prisma.order.create({
-//         data: {
-//           userId: userId,
-//           completed: req.body.completed, // Assuming the order is not completed initially
-//           productId: productId,
-//         },
-//       });
-  
-//       res.status(201).json({ message: 'Order created successfully', newOrder });
-//     } catch (error) {
-//       console.error('Error creating order:', error);
-//       res.status(500).json({ message: 'Internal server error' });
-//     } finally {
-//       await prisma.$disconnect();
-//     }
-//   };
-
 export const createOrder = async (req, res) => {
     try {
       const { userId, productId, completed } = req.body;
@@ -99,42 +68,3 @@ export const createOrder = async (req, res) => {
 
 
 
-// export const createOrder = async (req, res) => {
-//     try {
-//       const { userId, productId, completed } = req.body;
-  
-//       // Create a new order
-//       const newOrder = await prisma.order.create({
-//         data: {
-//           userId: userId,
-//           completed: completed || false,
-//           productId: productId,
-//         },
-//       });
-  
-//       // Check if the order is completed before updating user products
-//     if (completed) {
-//         // Update the count using Prisma update method
-//         await prisma.product.update({
-//             where: {
-//                 id: productId,
-//                 userId: userId
-//             },
-//             data: {
-//                 count: {
-//                     increment: 1
-//                 },
-//             },
-//         });
-
-//         console.log('Product count updated successfully');
-//     }
-  
-//       res.status(201).json({ message: 'Order created successfully', newOrder });
-//     } catch (error) {
-//       console.error('Error creating order:', error);
-//       res.status(500).json({ message: 'Internal server error' });
-//     } finally {
-//       await prisma.$disconnect();
-//     }
-//   };
